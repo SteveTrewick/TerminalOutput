1af8dfb01187adedd0f0af9b294bbb82bf58fcba
Steve Trewick <steve.trewick@icloud.com>
2025-10-06 18:40:55 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TerminalOutput
+
+A description of this package.
---
+++ Sources/TerminalOutput/TerminalOutput.swift
@@ -0,0 +1,6 @@
+public struct TerminalOutput {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
